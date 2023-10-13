80caad9372f0411032c047dde98407114b64e8f7
Másio César <72768168+MasioCesar@users.noreply.github.com>
2023-10-13 01:08:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function extrairLivros(input) {
   return livros;
 }
 
-function organizarLivrosEmEstantes(livros) {
+function alocarLivroNaPrateleira(livros) {
   const comprimentoEstante = 90;
   const estantes = [];
   let estanteAtual = [];
@@ -66,25 +66,60 @@ function organizarLivrosEmEstantes(livros) {
   return estantes;
 }
 
-function organizarLivros(estantesOrganizadas) {
+function reorganizarLivrosEmEstantes(livros) {
+  const estantes = [];
+  const comprimentoEstante = 90;
+
+  while (livros.length > 0) {
+    const estanteAtual = [];
+    const comprimentoPrateleiras = [0, 0, 0, 0, 0, 0];
+
+    for (let prateleiraAtual = 1; prateleiraAtual <= 6; prateleiraAtual++) {
+      const livrosAlocados = [];
+
+      for (let i = 0; i < livros.length; i++) {
+        const livro = livros[i];
+
+        if (comprimentoPrateleiras[prateleiraAtual - 1] + livro.comprimento <= comprimentoEstante) {
+          livro.estante = estantes.length + 1;
+          livro.prateleira = prateleiraAtual;
+          estanteAtual.push(livro);
+          comprimentoPrateleiras[prateleiraAtual - 1] += livro.comprimento;
+          livrosAlocados.push(i);
+        }
+      }
+
+      for (let j = livrosAlocados.length - 1; j >= 0; j--) {
+        livros.splice(livrosAlocados[j], 1);
+      }
+    }
+
+    if (estanteAtual.length > 0) {
+      estantes.push(estanteAtual);
+    }
+  }
+
+  return estantes;
+}
+
+function organizarAlfabeticamente(estantesOrganizadas) {
   for (let i = 0; i < estantesOrganizadas.length; i++) {
     const estante = estantesOrganizadas[i];
+
     estante.sort((a, b) => {
       if (a.prateleira === b.prateleira) {
+        if (a.titulo !== b.titulo) {
+          return a.titulo.localeCompare(b.titulo);
+        }
         if (a.altura !== b.altura) {
-          return b.altura - a.altura;
+          return a.altura - b.altura;
         }
         if (a.edicao !== b.edicao) {
           return a.edicao - b.edicao;
         }
-        if (a.ano !== b.ano) {
-          return a.ano - b.ano;
-        }
-        return a.titulo.localeCompare(b.titulo);
       }
     });
   }
-  estantesOrganizadas.reverse();
 }
 
 function mostrarEstantes(estantesOrganizadas) {
@@ -109,14 +144,17 @@ function mostrarEstantes(estantesOrganizadas) {
   }
 }
 
-const inputLivros = '/DAlculo 1,João da Silva,3,2022,25,30 | CAlculo 1,Maria Souza,2,2023,30,30 | Balculo 3,Carla Silva,2,2009,40,40\/Boa Noite,Marcos Pereira,1,2021,20,1 | Livro 1,Autor 1,1,2022,20,30 | Livro 2,Autor 2,1,2021,25,30 | Livro 3,Autor 3,1,2023,25,2\/Senhor dos Aneis,Tolkien,1,1950,20,45 | Senhor das Moscas,1,1970,30,40\/O grande gatsby,fitzgerald,1,1948,20,50\/Lego,Hasbro,1,2000,20,50\/Batman,DC,1,2000,20,40\/Homem de ferro,MARVEL,1,2000,24,60\/Jovem Tranquilao,Arthur,2,2012,20,50 | olamundo,Masio,2,2019,20,50\ ';
+const inputLivros = '/O Hobbit,J.R.R. Tolkien,1,1937,19,27 | 1984,George Orwell,1,1949,20,28 | Dom Quixote,Miguel de Cervantes,1,1605,23,30\/Dom Quixote,Miguel de Cervantes,2,1615,23,30 | O Senhor dos Anéis,J.R.R. Tolkien,1,1954,22,45 | A Revolução dos Bichos,George Orwell,1,1945,19,25\/Crime e Castigo,Fyodor Dostoevsky,1,1866,21,30 | Ulisses,James Joyce,1,1922,24,34 | Os Miseráveis,Victor Hugo,1,1862,20,36 | Moby Dick,Herman Melville,1,1851,23,35\/Senhor dos Aneis,Tolkien,1,1950,20,45 | Senhor das Moscas,1,1970,30,40\/O grande gatsby,fitzgerald,1,1948,20,50\/Lego,Hasbro,1,2000,20,5\/Batman,DC,1,2000,20,28\/Homem de ferro,MARVEL,1,2000,24,5\/Jovem Tranquilao,Arthur,2,2012,20,20 | Ola Mundo,Masio,2,2019,20,20\ ';
+
 const livros = extrairLivros(inputLivros);
-const estantesOrganizadas = organizarLivrosEmEstantes(livros);
+let estantesOrganizadas = alocarLivroNaPrateleira(livros);
 
 console.log("\nESTANTES ANTES DE ORGANIZAR:\n")
 mostrarEstantes(estantesOrganizadas);
 
-organizarLivros(estantesOrganizadas);
+estantesOrganizadas = reorganizarLivrosEmEstantes(livros);
+
+organizarAlfabeticamente(estantesOrganizadas);
 
 console.log("\nESTANTES APÓS ORGANIZAR:\n")
 mostrarEstantes(estantesOrganizadas);
